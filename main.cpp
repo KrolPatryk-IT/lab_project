17555d703ce3f9d36d0ff91befa0ef9e8683a4ce
KrolPatryk-IT <Krol_Patryk@protonmail.com>
2020-04-25 12:41:37 +0200

    Add menu

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main() {
 
-    cout << "Program sluzy do wykonywania dzialan matematycznych";
+    cout << "Program sluzy do wykonywania dzialan matematycznych\n";
+
+    cout << "Wybierz numer dzialania:\n";
+    cout << "\t1 - Dodawanie\n";
+    cout << "\t2 - Odejmowanie\n";
+    cout << "\t3 - Mnozenie\n";
+    cout << "\t4 - Dzielenie\n";
+
 
     return 0;
 }
